3841e902d67725e61a2da5122fc05910413c7026
papu-hacker <hrishikesh.patel@zohomail.in>
2026-04-05 14:38:58 +0530

    creat-react-app vs react-vite-app

---
+++ 01_basic-react-app/src/Chai.js
@@ -0,0 +1,6 @@
+function Chai() {
+    return(
+        <h2>hehe</h2>
+    )
+}
+export default Chai
---
+++ 01_react-vite-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    <>
+    <h1> Chai Aur React Vite </h1>
+    <Chai />
+    </>
+  )
+}
+
+export default App
